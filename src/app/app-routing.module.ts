f5f0043cec716612e3aa68f6feed5879915a3c24
Aniket Kale <kaleaniket66@gmail.com>
2023-04-09 16:08:51 +0530

    chore: add submissions module & header component with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'submittions',
+    pathMatch: 'full'
+  },
+  {
+    path: 'submittions',
+    loadChildren: () => import('./submissions/submissions.module').then(m => m.SubmissionsModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'submittions',
+    pathMatch: 'full'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
